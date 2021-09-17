5bec7bcdab8dd1310813a1381a77130e4debc2e1
LiYu <2248868140@qq.com>
2021-09-17 09:14:31 +0800

    完成基本架构

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <footer>
+            脚
+        </footer>
+    )
+}
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderStyle = styled.header`
+    background: #02101f;
+    color: white;
+    display: flex;
+     padding: 10px 100px;
+    align-items: center;
+`
+const StyleLink = styled(NavLink)`
+  margin-left: 34px;
+  &.selected{
+    border-bottom: 1px solid white;
+  }
+`
+const Header = () => {
+    return (
+        <HeaderStyle>
+            <div>logo</div>
+            <nav>
+                <StyleLink to="/" activeClassName="selected" exact>首页</StyleLink>
+                <StyleLink to="/history" activeClassName="selected" exact>上传历史</StyleLink>
+                <StyleLink to="/about" activeClassName="selected" exact>关于我</StyleLink>
+            </nav>
+        </HeaderStyle>
+    )
+}
+export default Header
